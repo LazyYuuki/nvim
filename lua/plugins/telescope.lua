14904b58a82d63d41c060168cf4fd39f6c445cec
LazyYuuki <huy2840@gmail.com>
2024-03-23 15:07:49 +0800

    Update with mason and indent

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,39 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require("telescope").setup({
-      defaults = {
-        layout_config = {
-          horizontal = {
-            preview_cutoff = 0, 
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_config = {
+            horizontal = {
+              preview_cutoff = 0, 
+            }
           }
         }
-      }
-    })
+      })
 
-    local builtin = require('telescope.builtin')
+      local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      local tele = require("telescope")
+      tele.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
+      })
+
+      tele.load_extension("ui-select")
+    end
+  }
 }
