b5880e7e151e5085dc15aa837e7c79e663fcf9c8
LazyYuuki <huy2840@gmail.com>
2024-03-24 20:39:25 +0800

    nothing

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,40 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
         defaults = {
           layout_config = {
             horizontal = {
-              preview_cutoff = 0, 
-            }
-          }
-        }
+              preview_cutoff = 0,
+            },
+          },
+        },
       })
 
-      local builtin = require('telescope.builtin')
+      local builtin = require("telescope.builtin")
 
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       local tele = require("telescope")
       tele.setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
       })
 
       tele.load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
