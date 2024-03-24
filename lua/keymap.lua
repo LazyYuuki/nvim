00c8633020dd79af37b0415aecc38ca8f0f31f03
LazyYuuki <huy2840@gmail.com>
2024-03-24 11:43:26 +0800

    Add motion to terminal

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,3 +4,4 @@ vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<Cr>") -- toggle nvim-tree
 vim.keymap.set({ "n", "v" }, "<", "<h") -- shift left
 vim.keymap.set({ "n", "v" }, ">", ">l") -- shift right
+vim.keymap.set("t", "jk", [[<C-\><C-n>]]) -- go to normal mode from insert mode in terminal
