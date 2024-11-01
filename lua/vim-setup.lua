8e40160de2ee386ed9f757cda9b462590662bec0
LazyYuuki <huy2840@gmail.com>
2024-11-01 21:39:41 +0800

    🎨 Column stuff

--- lua/vim-setup.lua
+++ lua/vim-setup.lua
@@ -2,11 +2,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=0")
 vim.cmd("set expandtab")
-vim.cmd("set list")
 vim.cmd("set lcs+=space:·")
 vim.cmd("set number relativenumber")
+
+-- vim.cmd("set list")
 vim.cmd("set linebreak")
--- vim.cmd("set wrapmargin=2")
+vim.cmd("set wrap")
+vim.cmd("set columns=120")
+
 vim.cmd("runtime macros/matchit.vim")
 vim.cmd("filetype plugin on")
 vim.opt.cursorline = true
