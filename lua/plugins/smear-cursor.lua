e8448c2428f9c247a8bb684429f9f26b5280c167
LazyYuuki <huy2840@gmail.com>
2026-02-01 17:23:05 +0800

    🐛 fix: smear-cursor to smear_cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,7 +1,7 @@
 return {
   "sphamba/smear-cursor.nvim",
   config = function()
-    require("smear-cursor").setup({
+    require("smear_cursor").setup({
       stiffness = 0.8,
       trailing_stiffness = 0.5,
       distance_stop_animating = 0.5
