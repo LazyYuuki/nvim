02a1e864168ceec64caf6925b73641adbaf36ca4
LazyYuuki <huy2840@gmail.com>
2026-02-01 21:57:48 +0800

    ✨ feat: add agentic.lua and opencode.lua (experimental)

---
+++ lua/plugins/agentic.lua
@@ -0,0 +1,11 @@
+return {
+  "carlos-algms/agentic.nvim",
+  opts = {
+    provider = "opencode-acp",
+    diff_preview = {
+      enabled = true,
+      layout = "inline",
+      center_on_navigate_hunks = true
+    },
+  },
+}
